bc0f2bc0d070f43c8486e0fb1075f6c12469be5e
Crowdbotics <team@crowdbotics.com>
2020-12-15 23:43:40 +0000

    Update models

--- backend/clickup_test_2_23396/wsgi.py
+++ backend/clickup_test_2_23396/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clickup_test_2_23396.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clickup_test_2_23396.settings")
 
 application = get_wsgi_application()
